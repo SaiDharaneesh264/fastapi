a328f21b6058cfb8c8457b509ad77291c9b6a4d8
sai dharaneesh <saidharaneesh@sais-MacBook-Pro.local>
2022-10-03 02:40:58 +0530

    blog fastapi

---
+++ blog/schemas.py
@@ -0,0 +1,47 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+class BlogBase(BaseModel):
+    title: str
+    body: str
+
+
+class Blog(BlogBase):
+    class Config():
+        orm_mode = True
+
+class User(BaseModel):
+    name: str
+    password: str
+    email: str
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
+    blogs: List[Blog] = []
+    class Config():
+        orm_mode = True
+
+class ShowUserDetails(BaseModel):
+    name: str
+    email: str
+    class Config():
+        orm_mode = True
+
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+    creator: ShowUserDetails
+    class Config():
+        orm_mode = True
+
+class Login(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    email: Optional[str] = None
